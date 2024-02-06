c8ea0183d74cf664746bc24f578fb54df0c65d38
Vladislav Sergeev <vssx04@gmail.com>
2024-02-06 19:18:36 +0700

    initial

---
+++ src/main/java/org/example/CdoTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.example.ui.CdoPage;
+
+import org.junit.jupiter.api.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class CdoTest {
+
+    @Test
+    public void Test() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://moodle-cdo.kemsu.ru/login/index.php");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        CdoPage cdoPage = new CdoPage(driver);
+
+        cdoPage.waitForElement("//*[@name='username']");
+        cdoPage.typeLogin("//*[@name='username']");
+        cdoPage.waitForElement("//*[@name='password']");
+        cdoPage.typePassword("//*[@name='password']");
+        cdoPage.waitForElement("//*[@class='btn btn-primary btn-block mt-3']");
+        cdoPage.waitForElement("//*[@class='card-img dashboard-card-img']");
+    }
+}
